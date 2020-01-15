704cef4fe747b0eb4e20c5d6d1a1e4e80b8540d1
Tyler <tyler.powell@temple.edu>
2020-01-15 16:39:01 -0500

    views change

--- serverNamingApp/views.py
+++ serverNamingApp/views.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Tech with Tyler!")
 # Create your views here.
