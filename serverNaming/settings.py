cdeb6eb0dbfd8b953488463f5ab241b90ab3bb4b
Tyler <tyler.powell@temple.edu>
2020-01-15 15:54:22 -0500

    update settings

--- serverNaming/settings.py
+++ serverNaming/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'serverNamingApp'
+    'serverNamingApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
